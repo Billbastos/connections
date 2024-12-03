6b2c8791611c2fa71ee1b645be07ae990b0b7ae1
Guilherme Bastos <gui.b.bastos@gmail.com>
2024-12-02 16:17:22 -0800

    initial game development

---
+++ src/App.tsx
@@ -0,0 +1,196 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import './App.css'
+import { data, groups } from './data'
+import shuttle from 'lodash/shuffle'
+
+const MAX_TERMS = 4
+const MAX_LIVES = 4
+
+function App() {
+  const [connections, setConnections] = useState(shuttle(data))
+  const [winner, setWinner] = useState(false)
+  const [lives, setLives] = useState(MAX_LIVES)
+  const refs = [
+    useRef<HTMLTableRowElement>(null),
+    useRef<HTMLTableRowElement>(null),
+    useRef<HTMLTableRowElement>(null),
+    useRef<HTMLTableRowElement>(null),
+  ]
+
+  useEffect(() => {
+    if (connections.some((c) => !c.connected)) {
+      return
+    }
+    setWinner(true)
+  }, [connections])
+
+  function handleCellClick(item: (typeof data)[0]) {
+    const checkedItems = connections.filter((i) => i.checked)
+    if (checkedItems.length === MAX_TERMS && !item.checked) {
+      return
+    }
+
+    if (item.connected) {
+      return
+    }
+
+    setConnections(
+      connections.map((i) => {
+        if (i.id === item.id) {
+          return { ...i, checked: !i.checked }
+        }
+        return i
+      })
+    )
+  }
+
+  function handleConnect() {
+    const checkedItems = connections
+      .filter((i) => i.checked)
+      .reduce((acc: number[], i: (typeof data)[0]) => {
+        return [...acc, i.id]
+      }, [])
+    let isCorrect = false
+
+    for (let i = 0; i < groups.length; i++) {
+      const group = JSON.stringify(groups[i].connections.sort((a, b) => a - b))
+      const checked = JSON.stringify(checkedItems.sort((a, b) => a - b))
+      if (group === checked) {
+        isCorrect = true
+        break
+      }
+    }
+
+    if (!isCorrect) {
+      setLives(lives - 1)
+      return
+    }
+
+    moveRowToTop(checkedItems)
+  }
+
+  function moveRowToTop(checkedItems: number[]) {
+    const newMap = connections.map((i) => {
+      if (checkedItems.includes(i.id)) {
+        return { ...i, connected: true, checked: false }
+      }
+      return i
+    })
+    const allConnected = newMap.filter((i) => i.connected)
+    setConnections([
+      ...allConnected,
+      ...newMap.filter((i) => i.connected === false),
+    ])
+  }
+
+  function getColor(item: (typeof data)[0]) {
+    if (item.checked) {
+      return {
+        background: '#333',
+        color: 'white',
+      }
+    }
+    if (item.connected) {
+      return {
+        background: groups.find((g) => g.connections.includes(item.id))?.color,
+        color: '#333',
+      }
+    }
+    return {
+      background: 'transparent',
+      color: '#333',
+    }
+  }
+
+  function restart() {
+    setConnections(data)
+    setWinner(false)
+    setLives(MAX_LIVES)
+  }
+
+  const displayConnectedMessage = useCallback(() => {
+    const connected = connections.filter((i) => i.connected)
+    const nodes: React.ReactNode[] = []
+
+    for (let i = 0; i < connected.length / MAX_TERMS; i++) {
+      const group = groups.find((g) =>
+        g.connections.includes(connected[i * MAX_TERMS].id)
+      )
+      const rect = refs[i].current?.getBoundingClientRect()
+      const width = rect?.width ?? 0
+      const top = rect?.bottom ?? 0
+      const left = rect?.left ?? 0
+      nodes.push(
+        <div className='pill-container' style={{ width, top: top - 25, left }}>
+          <span key={connected[i * MAX_TERMS].id} className='pill'>
+            {group?.message}
+          </span>
+        </div>
+      )
+    }
+
+    return nodes
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [connections])
+
+  if (lives === 0) {
+    return (
+      <main>
+        <h1>Game Over 👎🏼</h1>
+        <h2>Try again</h2>
+        <button onClick={restart}>restart</button>
+      </main>
+    )
+  }
+
+  return (
+    <main>
+      <h1>Family connections {winner && ` - Congrats 🎉`}</h1>
+      <div className='game-wrapper'>
+        <div className='table-container'>
+          <table>
+            <tbody>
+              {Array.from([0, 1, 2, 3]).map((item) => (
+                <tr key={item} ref={refs[item]}>
+                  {connections
+                    .slice(item * MAX_TERMS, item * MAX_TERMS + MAX_TERMS)
+                    .map((i) => (
+                      <td
+                        key={i.name}
+                        style={{ ...getColor(i) }}
+                        onClick={() => handleCellClick(i)}
+                      >
+                        {i.name}
+                      </td>
+                    ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        {winner ? (
+          <button onClick={restart}>restart</button>
+        ) : (
+          <>
+            <div className='lives'>
+              {Array.from({ length: lives }).map((_, index) => (
+                <span key={index}>❤️</span>
+              ))}
+            </div>
+            <button
+              disabled={
+                connections.filter((i) => i.checked).length !== MAX_TERMS
+              }
+              onClick={handleConnect}
+            >
+              Connect
+            </button>
+          </>
+        )}
+      </div>
+      {displayConnectedMessage()}
+    </main>
+  )
+}
+
+export default App
---
+++ src/data.ts
@@ -0,0 +1,119 @@
+export const groups = [
+  {
+    connections: [1, 2, 3, 4],
+    message: 'Parabéns. Vocês merecem muito pelo esforço que fizeram este ano.',
+    color: '#B272E0',
+  },
+  { connections: [5, 6, 7, 8], message: 'Membros do x-men', color: '#4690EB' },
+  {
+    connections: [9, 10, 11, 12],
+    message: 'Iniciais dos times da premier league',
+    color: '#52C878',
+  },
+  {
+    connections: [13, 14, 15, 16],
+    message: 'Um dos personagens da dupla dos programas de criança',
+    color: '#FFDB58',
+  },
+]
+
+export const data = [
+  {
+    id: 13,
+    name: 'buzz',
+    checked: false,
+    connected: false,
+  },
+  {
+    id: 1,
+    name: 'tuition',
+    checked: false,
+    connected: false,
+  },
+  {
+    id: 2,
+    name: 'is',
+    checked: false,
+    connected: false,
+  },
+  {
+    id: 7,
+    name: 'jean',
+    checked: false,
+    connected: false,
+  },
+  {
+    id: 8,
+    name: 'logan',
+    checked: false,
+    connected: false,
+  },
+  {
+    id: 9,
+    name: 'man',
+    checked: false,
+    connected: false,
+  },
+  {
+    id: 10,
+    name: 'liver',
+    checked: false,
+    connected: false,
+  },
+  {
+    id: 11,
+    name: 'new',
+    checked: false,
+    connected: false,
+  },
+  {
+    id: 12,
+    name: 'west',
+    checked: false,
+    connected: false,
+  },
+  {
+    id: 3,
+    name: 'fully',
+    checked: false,
+    connected: false,
+  },
+  {
+    id: 15,
+    name: 'sullivan',
+    checked: false,
+    connected: false,
+  },
+  {
+    id: 6,
+    name: 'hank',
+    checked: false,
+    connected: false,
+  },
+  {
+    id: 5,
+    name: 'scott',
+    checked: false,
+    connected: false,
+  },
+  {
+    id: 4,
+    name: 'paid',
+    checked: false,
+    connected: false,
+  },
+
+  {
+    id: 14,
+    name: 'zick',
+    checked: false,
+    connected: false,
+  },
+
+  {
+    id: 16,
+    name: 'phineas',
+    checked: false,
+    connected: false,
+  },
+]
